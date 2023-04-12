b5755b5baac04cb2d7895cf524366da6f6d0c014
+ <+>
2023-04-12 16:35:40 -0400

    completed phase 1 scope lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bob sucks'
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// my original thought process was to create the function
+// upperCaseCustomerName() {
+//    and within the function, return customerName.toUpperCase();
+//}
+
+
+// Write a function that when called, declares a variable called 
+// bestCustomer in global scope and assigns it to be 'not bob'
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'keon'
+}
